c403ca003764588476cc610ba86496889f1bbb0d
Larry Bell <larry.bell.pmp@gmail.com>
2020-01-22 22:30:12 -0500

    changes to menu and toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,7 +8,6 @@ import classes from './Toolbar.module.css';
 
 const toolbar = (props) => (
     <header className = {classes.Toolbar}>
-        <div>MENU</div>
         <div className={classes.Logo}>
             <Logo />
         </div>
